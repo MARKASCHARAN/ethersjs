500f71c202ff81df7ba038aef4770472534bbd46
MARKASCHARAN <markascharan@gmail.com>
2025-05-13 10:42:05 +0530

    Id

--- examples/6_block_inspect.js
+++ examples/6_block_inspect.js
@@ -1,6 +1,6 @@
 const {ethers} = require("ethers");
 
-const INFURA_ID = "6903fada217248bd8292fd3068e4c27a";
+
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
 
 const main = async () =>{
