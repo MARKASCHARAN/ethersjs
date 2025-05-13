3b9f56d62426721909ce21407b67ef6c78694674
MARKASCHARAN <markascharan@gmail.com>
2025-05-13 08:59:39 +0530

    4th

--- examples/4_write_smart_cont.js
+++ examples/4_write_smart_cont.js
@@ -1,10 +1,10 @@
 const { ethers } = require("ethers");
-const INFURA_ID = "abc";
+const INFURA_ID = "";
 
 const provider = new ethers.providers.JsonRpcProvider(`https://sepolia.infura.io/v3/${INFURA_ID}`);
 
-const acnt1 = "";
-const acnt2 = "";
+const acnt1 = "0x3CCd00Da039d68d6F169e11A7739ea4d6Be96159";
+const acnt2 = "0x23646131b3f5aB80DBFfbdB478bBDc17fB9B9FC7";
 
 const privateKey1 = "";
 
@@ -22,6 +22,20 @@ const contract = new ethers.Contract(address, ERC20_ABI, provider);
 
 const main = async () => {
 
+const balance = await contract.balanceOf(acnt1);
+console.log(`Balance of ${acnt1}: ${balance}`);
+const balance2 = await contract.balanceOf(acnt2);
+console.log(`Balance of ${acnt2}: ${balance2}`);
+
+ const connectWallet = contract.connect(wallet)
+ const txn =  await connectWallet.transfer(acnt2, balance);
+ await txn.wait();
+ console.log(txn);
+
+ const balanceAfter1 = await contract.balanceOf(acnt1);
+ console.log(`Balance of ${acnt1} after transfer: ${balanceAfter1}`);
+ const balanceAfter2 = await contract.balanceOf(acnt2);
+ console.log(`Balance of ${acnt2}: ${balanceAfter2}`);
 }
 
 main()
